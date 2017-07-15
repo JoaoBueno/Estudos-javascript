8455bff791e5c2adca44c1abaf90a0db7f9feb07
Bueno <heavyhide@gmail.com>
2017-07-15 07:31:40 -0300

    Ajax Electron Vue

---
+++ vue/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <hello></hello>
+    <input-email></input-email>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+import InputEmail from './components/InputEmail'
+
+export default {
+  name: 'app',
+  components: {
+    Hello,
+    InputEmail
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
