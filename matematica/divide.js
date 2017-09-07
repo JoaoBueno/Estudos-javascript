012fbc25a96e50f5997d2034c8739d402c9a1ede
Heisenberg <lucaslopes.t@gmail.com>
2017-09-06 21:24:34 -0300

    Update divide.js
    Outra possível implementação...

--- matematica/divide.js
+++ matematica/divide.js
@@ -15,3 +15,17 @@ const divide = (a, b) => {
 }
 
 console.log(divide(10, 2))
+// possível implementação..
+// sem erros...
+function div(N, D) {
+	return D === 0 || N < D 
+		? 0
+		: N - D === 0
+			? 1
+			: 1 + div(N-D, D)
+ }
+
+console.log(
+	div(10, 5) === 2,  // true
+	div(10, 1) === 10 // true
+);
