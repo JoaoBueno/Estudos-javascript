593925a57d51dcee90d79603f5d1cb5ceef90cb7
Suissa <jnascimento@gmail.com>
2017-09-08 20:09:19 -0300

    corrigido

--- express-atomic/modules/Pokemon/routes.js
+++ express-atomic/modules/Pokemon/routes.js
@@ -1,44 +1,44 @@
 var express = require('express')
 var router = express.Router()
 
-// const Controller = require('../pokemon-controller')
+const Controller = require('./controller')
 // console.log(Controller)
 
-const Schema = require('./schema')
-const Model = require('./model')(Schema, 'Pokemon')
+// const Schema = require('./schema')
+// const Model = require('./model')(Schema, 'Pokemon')
 
-const Controller = {
-  create: function(data) {
-    console.log('data', data)
-    const Suissamon = new Model(data)
-    console.log('suissamon', Suissamon)
-    Suissamon.save(function(err, data) {
-      if (err) return console.log('Erro:', err)
-      return console.log('Inseriu:', data)
-    })
-  },
+// const Controller = {
+//   create: function(data) {
+//     console.log('data', data)
+//     const Suissamon = new Model(data)
+//     console.log('suissamon', Suissamon)
+//     Suissamon.save(function(err, data) {
+//       if (err) return console.log('Erro:', err)
+//       return console.log('Inseriu:', data)
+//     })
+//   },
 
-  retrieve: function(res, query) {
-    // console.log('res', res)
-    Model.find(query).exec()
-        .then(data => res.json(data))
-        .catch(err => res.json(err))
-  },
+//   retrieve: function(res, query) {
+//     // console.log('res', res)
+//     Model.find(query).exec()
+//         .then(data => res.json(data))
+//         .catch(err => res.json(err))
+//   },
 
-  update: function(query, mod, options) {
-    options = options || {}
-    Model.update(query, mod, options, function(err, data) {
-      if (err) return console.log('Erro:', err)
-      return console.log('Alterou:', data)
-    })
-  },
-  delete: function(query) {
-    Model.remove(query, function(err, data) {
-      if (err) return console.log('Erro:', err)
-      return console.log('Deletou:', data)
-    })
-  }
-}
+//   update: function(query, mod, options) {
+//     options = options || {}
+//     Model.update(query, mod, options, function(err, data) {
+//       if (err) return console.log('Erro:', err)
+//       return console.log('Alterou:', data)
+//     })
+//   },
+//   delete: function(query) {
+//     Model.remove(query, function(err, data) {
+//       if (err) return console.log('Erro:', err)
+//       return console.log('Deletou:', data)
+//     })
+//   }
+// }
 
 
 const list = function(req, res, next) {
