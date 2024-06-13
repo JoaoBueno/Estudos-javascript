c0775cab6f07043879c23c3c0dfa662ce963249a
João Carlos Bueno Masso <heavyhide@gmail.com>
2024-06-13 13:53:00 -0300

    Apagando Node Modules

--- matematica/todosiguais.js
+++ matematica/todosiguais.js
@@ -1,23 +1,23 @@
-function todosIguais ( array ) {
-  const filtra = array.filter( function ( elem, pos, arr ) {
-    return arr.indexOf( elem ) == pos
-  } )
-  return filtra.length === 1
-}
+// function todosIguais ( array ) {
+//   const filtra = array.filter( function ( elem, pos, arr ) {
+//     return arr.indexOf( elem ) == pos
+//   } )
+//   return filtra.length === 1
+// }
 
-todosIguais = ( array ) => {
-  const filtra = array.filter( function ( elem, pos, arr ) {
-    return arr.indexOf( elem ) == pos
-  } )
-  return filtra.length === 1
-}
+// todosIguais = ( array ) => {
+//   const filtra = array.filter( function ( elem, pos, arr ) {
+//     return arr.indexOf( elem ) == pos
+//   } )
+//   return filtra.length === 1
+// }
 
-todosIguais = ( array ) => {
-  const filtra = array.filter( ( elem, pos, arr ) => {
-    return arr.indexOf( elem ) == pos
-  } )
-  return filtra.length === 1
-}
+// todosIguais = ( array ) => {
+//   const filtra = array.filter( ( elem, pos, arr ) => {
+//     return arr.indexOf( elem ) == pos
+//   } )
+//   return filtra.length === 1
+// }
 
 todosIguais = ( array ) => {
   const filtra = array.filter( ( elem, pos, arr ) => {
@@ -28,3 +28,8 @@ todosIguais = ( array ) => {
   return filtra.length === 1
 }
 
+console.log( todosIguais( [ 1, 2, 3 ] ) )
+console.log( todosIguais( [ 0, 1, 2 ] ) )
+console.log( todosIguais( [ 0, 2, 3 ] ) )
+console.log( todosIguais( [ 1, 1, 1, 1 ] ) )
+console.log( todosIguais( [ 4, 4, 4, 4, 3 ] ) )
